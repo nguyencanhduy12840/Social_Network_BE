152bf7e45e281a5281f9ad69dad33052f7a42077
nguyencanhduy12840 <huyduynguyen12@gmail.com>
2025-10-16 22:34:39 +0700

    Config project

---
+++ monitoradmin/src/main/java/com/socialapp/monitoradmin/MonitoradminApplication.java
@@ -0,0 +1,13 @@
+package com.socialapp.monitoradmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoradminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoradminApplication.class, args);
+	}
+
+}
