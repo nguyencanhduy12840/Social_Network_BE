d7fac126dfe860adc9e6bb472110811f7a24b900
nguyencanhduy12840 <huyduynguyen12@gmail.com>
2025-11-11 22:37:25 +0700

    fix

--- notificationservice/src/main/java/com/socialapp/notificationservice/kafka/PostEventListener.java
+++ notificationservice/src/main/java/com/socialapp/notificationservice/kafka/PostEventListener.java
@@ -1,25 +1,25 @@
-package com.socialapp.notificationservice.kafka;
+// package com.socialapp.notificationservice.kafka;
 
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Service;
+// import org.springframework.kafka.annotation.KafkaListener;
+// import org.springframework.stereotype.Service;
 
 
-import com.socialapp.notificationservice.dto.PostEventDTO;
-import com.socialapp.notificationservice.service.NotificationService;
+// import com.socialapp.notificationservice.dto.PostEventDTO;
+// import com.socialapp.notificationservice.service.NotificationService;
 
-import lombok.extern.slf4j.Slf4j;
+// import lombok.extern.slf4j.Slf4j;
 
-@Service
-@Slf4j
-public class PostEventListener {
-     private final NotificationService notificationService;
-    public PostEventListener(NotificationService notificationService) {
-        this.notificationService = notificationService;
-    }
+// @Service
+// @Slf4j
+// public class PostEventListener {
+//      private final NotificationService notificationService;
+//     public PostEventListener(NotificationService notificationService) {
+//         this.notificationService = notificationService;
+//     }
 
-    @KafkaListener(topics = "post-events", groupId = "notification-group")
-    public void consume(PostEventDTO event) {
-        log.info("Received post event: {}", event);
-        notificationService.processPostEvent(event);
-    }
-}
+//     @KafkaListener(topics = "post-events", groupId = "notification-group")
+//     public void consume(PostEventDTO event) {
+//         log.info("Received post event: {}", event);
+//         notificationService.processPostEvent(event);
+//     }
+// }
---
+++ notificationservice/src/main/java/com/socialapp/notificationservice/mapper/FriendEventConverter.java
@@ -0,0 +1,15 @@
+// package com.socialapp.notificationservice.mapper;
+
+// import org.modelmapper.ModelMapper;
+// import org.springframework.stereotype.Component;
+
+// @Component
+// public class FriendEventConverter {
+//     private final ModelMapper modelMapper;
+
+//     public FriendEventConverter(ModelMapper modelMapper) {
+//         this.modelMapper = modelMapper;
+//     }
+
+//     public 
+// }
