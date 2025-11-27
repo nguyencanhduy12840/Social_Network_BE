8067d84b202114aab861ad9dcd8d0be2c9fdd360
nguyencanhduy12840 <huyduynguyen12@gmail.com>
2025-11-27 22:02:40 +0700

    create dto update profile

---
+++ profileservice/src/main/java/com/socialapp/profileservice/dto/request/UpdateUserProfileRequest.java
@@ -0,0 +1,22 @@
+package com.socialapp.profileservice.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UpdateUserProfileRequest {
+    String firstName;
+    String lastName;
+    String username;
+    String avatarUrl;
+    String bio;
+    String gender;
+    LocalDate dob;
+}
