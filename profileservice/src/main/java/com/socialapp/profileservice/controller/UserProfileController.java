f635ec56f4597ddee22cd5343c639db9a443c6cd
NHHIEN <hien20040908@gmail.com>
2025-11-29 19:40:15 +0700

    refactor  code

--- profileservice/src/main/java/com/socialapp/profileservice/controller/UserProfileController.java
+++ profileservice/src/main/java/com/socialapp/profileservice/controller/UserProfileController.java
@@ -1,6 +1,5 @@
 package com.socialapp.profileservice.controller;
 
-;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.socialapp.profileservice.dto.request.UpdateUserProfileRequest;
@@ -12,7 +11,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
